2e41e0a54addf52e81b81384f2eca94be8d2cd46
Faeyndal <5844057+Faeyndal@users.noreply.github.com>
2025-10-22 21:34:40 -0400

    Add page layout and initial grid

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+let gridSize = 4;
 
-
-function createGrid (gridSize) {
+function createGrid () {
     const grid = document.querySelector(".gridContainer");
 
-    const gridTile = document.createElement('div');
-
-    for (let i = 0; i < gridSize; i++ ){
-        grid.appendChild(gridTile);
+    for (let c = 0; c < gridSize; c++){
+        const row = document.createElement('div');
+        row.classList.add("gridRow");
+        for (let i = 0; i < gridSize; i++ ){
+            const tile = document.createElement('div');
+            tile.classList.add("gridTile");
+            
+            row.appendChild(tile);
+        }
+        grid.appendChild(row);
     }
 
 }
+
+
+createGrid(gridSize);
