11b1ef76dfab5ee90dffa5649fead0cf4d7e2a26
Faeyndal <5844057+Faeyndal@users.noreply.github.com>
2025-11-04 18:40:10 -0500

    Added grid wipe, sizing and basic styling

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let gridSize = 4;
 
-function createGrid () {
+function createGrid (gridSize = 5) {
     const grid = document.querySelector(".gridContainer");
+
+    console.log(gridSize);
 
     for (let c = 0; c < gridSize; c++){
         const row = document.createElement('div');
+
         row.classList.add("gridRow");
+
         for (let i = 0; i < gridSize; i++ ){
             const tile = document.createElement('div');
+
             tile.classList.add("gridTile");
-            
+            tile.style.width = (800/gridSize) + 'px';
+            tile.style.height = (800/gridSize) + 'px';
+            tile.addEventListener("mouseover", colorTile);
+
             row.appendChild(tile);
         }
         grid.appendChild(row);
@@ -17,5 +24,30 @@ function createGrid () {
 
 }
 
+createGrid();
 
-createGrid(gridSize);
+function resetGrid() {
+    let grid = document.querySelector('.gridContainer');
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+const resetButton = document.querySelector('.resetButton');
+
+resetButton.addEventListener('click', () => {
+    let gridSize = Number(prompt('Size of new grid? (max 100)'));
+    
+    if (gridSize > 100 || gridSize < 1 || isNaN(gridSize)) {
+        alert("Size must be a number between 1 and 100");
+        return;
+    }
+    resetGrid();
+    createGrid(gridSize);
+
+});
+
+function colorTile(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
